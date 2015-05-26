5dd0a945903eac5634b5d2dd6e2a431fa344e2b0
Harsimran Singh <me@harsimransingh.in>
2015-05-26 07:46:01 +0530

    Added test for most functions

--- test_wordt.py
+++ test_wordt.py
@@ -8,5 +8,30 @@ class WordTreasureTestCase(unittest.TestCase):
         display_definitions(word)
         return True
 
+    def test_random_words(self):
+        limit = 10
+        display_random_words(limit)
+        return True
+    def test_display_examples(self):
+        limit = 10
+        word = "hello"
+        display_examples(word, limit)
+        return True
+    def test_display_top_examples(self):
+        word = "hello"
+        display_top_examples(word)
+        return True
+    def test_display_related_words(self):
+        word = "hello"
+        display_related_words(word)
+        return True
+    def test_display_compact(self):
+        word = "hello"
+        display_compact(word)
+        return True
+    def test_help_display(self):
+        display_help()
+        return True
+
 if __name__=='__main__':
     unittest.main()
